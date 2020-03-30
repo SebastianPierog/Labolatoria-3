c47a78641e488f1381c81651b0a1933f1f9ed55e
Lenovo <Lenovo@DESKTOP-VJMISMD>
2020-03-30 12:17:15 +0200

    Dodaj pliki projektów.

---
+++ Labolatoria3/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Labolatoria3
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Student>()
+                .Property(x => x.FirstName)
+                .HasMaxLength(255)
+                .IsRequired();
+
+
+            modelBuilder.Entity<Student>()
+                .Property(x => x.LastName)
+                .HasMaxLength(255);
+        }
+    }
+}
